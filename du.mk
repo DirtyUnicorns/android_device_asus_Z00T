b7ecb77a9347b40876cbbde266098e184aa3a7ba
nickdoh81 <nickdoh81@gmail.com>
2016-08-04 23:18:38 -0400

    Override ro.product.device to ASUS_Z00T for pixelmaster
    Change-Id: Ibb5557a5edb7c553ffeb528a026accfd13fe3f85

--- du.mk
+++ du.mk
@@ -3,6 +3,10 @@ $(call inherit-product, device/asus/Z00T/full_Z00T.mk)
 # Inherit some common DU stuff.
 $(call inherit-product, vendor/du/config/common_full_phone.mk)
 
+# set for pixel master camera
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=ASUS_Z00T
+
 PRODUCT_NAME := du_Z00T
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
